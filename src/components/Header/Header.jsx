dd3b1ef7dc297279c018515ba5d2a736085b659b
RM <rubelmollah@9311.com>
2024-03-03 22:00:23 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <nav className='flex justify-between mx-auto container mt-4 text-4xl'>
+                <div>Knowledge Cafe</div>
+                <div><img src="" alt="" />img</div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;

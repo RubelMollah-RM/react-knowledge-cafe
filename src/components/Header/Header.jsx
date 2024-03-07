95f13fda712604f07fc042071882a6d23b627ae0
RM <rubelmollah@9311.com>
2024-03-07 20:29:32 +0600

    added all components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+
 const Header = () => {
     return (
         <div>
             <nav className='flex justify-between mx-auto container mt-4 text-4xl'>
                 <div>Knowledge Cafe</div>
-                <div><img src="" alt="" />img</div>
+                <div><img src={profile.png} alt="" /></div>
             </nav>
         </div>
     );
